88a8cddcd8e788c49a33b28cc71f140e0ed035f4
Hinduraj8199 <hindurajkkadam123@gmail.com>
2021-06-19 16:03:16 +0530

    Initial Project Setup

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = `mongodb+srv://m001-student:m001-mongodb-basics@coursera.belfw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const PORT = process.env.port || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server unning on port ${PORT}`))
+  )
+  .catch((err) => console.log(err.message));
+
+mongoose.set("useFindAndModify", false);
